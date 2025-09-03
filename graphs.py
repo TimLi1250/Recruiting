0271ba4bff36b45ee1e56d6d1980f6bda603fb8e
Tim Lezun Li <tim.li0521@gmail.com>
2025-09-03 08:59:03 -0400

    dijkstras

--- graphs.py
+++ graphs.py
@@ -246,3 +246,49 @@ Here we can use Kahn's algorithm:
 * pop the course from the queue and then for each course that has course 0 as a prerequisite, reduce their in-degree by 1
 * then add the new courses who have an in-degree of 0 to the queue
 '''
+
+'''
+Let's take a look at Dijkstra's Algorithm
+'''
+
+class Graph:
+    def __init__(self, size):
+        self.adj_matrix = [[0] * size for _ in range(size)]
+        self.size = size
+        self.vertex_data = [''] * size
+
+    def add_edge(self, u, v, weight):
+        if 0 <= u < self.size and 0 <= v < self.size:
+            self.adj_matrix[u][v] = weight
+            self.adj_matrix[v][u] = weight  # For undirected graph
+
+    def add_vertex_data(self, vertex, data):
+        if 0 <= vertex < self.size:
+            self.vertex_data[vertex] = data
+
+    def dijkstra(self, start_vertex_data):
+        start_vertex = self.vertex_data.index(start_vertex_data)
+        distances = [float('inf')] * self.size
+        distances[start_vertex] = 0
+        visited = [False] * self.size
+
+        for _ in range(self.size):
+            min_distance = float('inf')
+            u = None
+            for i in range(self.size):
+                if not visited[i] and distances[i] < min_distance:
+                    min_distance = distances[i]
+                    u = i
+
+            if u is None:
+                break
+
+            visited[u] = True
+
+            for v in range(self.size):
+                if self.adj_matrix[u][v] != 0 and not visited[v]:
+                    alt = distances[u] + self.adj_matrix[u][v]
+                    if alt < distances[v]:
+                        distances[v] = alt
+
+        return distances
